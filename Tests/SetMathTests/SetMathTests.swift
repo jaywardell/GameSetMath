7993cc328671c3e9fff9f0baffc71061dc95c719
Joseph Wardell <jay@jaywardell.me>
2025-02-23 22:32:17 -0500

    formatting

--- Tests/SetMathTests/SetMathTests.swift
+++ Tests/SetMathTests/SetMathTests.swift
@@ -6,7 +6,6 @@ struct union_method {
 
     @Test("if set is empty, returns nil")
     func example_empty() async throws {
-        
         let data: [[String]] = []
         
         #expect(union(of: data).isEmpty)
@@ -14,7 +13,6 @@ struct union_method {
 
     @Test("if two collections have nothing in common, returns empty set")
     func example_two_disjoint() async throws {
-        
         let data = [
             ["a", "b"],
             ["c", "d"]
