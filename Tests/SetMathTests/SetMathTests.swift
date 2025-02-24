f9de47b6521a1b701dbec7debaf75e3daaef3f16
Joseph Wardell <jay@jaywardell.me>
2025-02-23 22:31:41 -0500

    added another base class test

--- Tests/SetMathTests/SetMathTests.swift
+++ Tests/SetMathTests/SetMathTests.swift
@@ -3,8 +3,17 @@ import Testing
 
 @Suite("union")
 struct union_method {
-    @Test("if two types have nothing in common, returns empty set")
-    func example() async throws {
+
+    @Test("if set is empty, returns nil")
+    func example_empty() async throws {
+        
+        let data: [[String]] = []
+        
+        #expect(union(of: data).isEmpty)
+    }
+
+    @Test("if two collections have nothing in common, returns empty set")
+    func example_two_disjoint() async throws {
         
         let data = [
             ["a", "b"],
