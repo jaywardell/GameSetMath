93c312ddaf886d20ae18888a124719caeecb2377
Joseph Wardell <jay@jaywardell.me>
2025-02-23 22:30:33 -0500

    created a test suite and added an explanation of the one test

--- Tests/SetMathTests/SetMathTests.swift
+++ Tests/SetMathTests/SetMathTests.swift
@@ -1,12 +1,16 @@
 import Testing
 @testable import SetMath
 
-@Test func example() async throws {
-    
-    let data = [
-        ["a", "b"],
-        ["c", "d"]
-    ]
-    
-    #expect(union(of: data).isEmpty)
+@Suite("union")
+struct union_method {
+    @Test("if two types have nothing in common, returns empty set")
+    func example() async throws {
+        
+        let data = [
+            ["a", "b"],
+            ["c", "d"]
+        ]
+        
+        #expect(union(of: data).isEmpty)
+    }
 }
